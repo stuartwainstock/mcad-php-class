a051d087c47928c93d6199b65d733ae2b727a0b7
Stuart Wainstock <stuart.wainstock@gmail.com>
2015-02-15 14:17:27 -0600

    MCAD PHP Class _s Theme Creation

---
+++ content.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package MCAD PHP CLASS
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php mcad_php_class_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'mcad-php-class' ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+		?>
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'mcad-php-class' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php mcad_php_class_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * MCAD PHP CLASS Theme Customizer
+ *
+ * @package MCAD PHP CLASS
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function mcad_php_class_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'mcad_php_class_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function mcad_php_class_customize_preview_js() {
+	wp_enqueue_script( 'mcad_php_class_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'mcad_php_class_customize_preview_js' );
